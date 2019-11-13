5ff22c81c7af3ba374a40e7b32a8e1576d77b255
melni <melni@10.9.106.12>
2019-11-12 23:25:04 -0500

    removal

--- Maze2/src/mazePrinter.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-public class mazePrinter{
-   public static void main (String[] args) throws IOException{
-      MazeSolver solve = new MazeSolver("maze1.txt");
-    
-    }
-}
--- Maze2/src/mazeTester.java
+++
@@ -1,9 +0,0 @@
-
-import java.io.*;
-public class mazeTester{
-   public static void main (String[] args) throws IOException{
-	   System.out.println("The Maze will solve from where S is to where E is");
-      MazeSolver solve = new MazeSolver("src\\Maze1.txt");
-    
-    }
-}
